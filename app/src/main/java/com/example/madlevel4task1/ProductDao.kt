9671d932ce379ed7bf39e39735d8ace5cd2b18bb
Niels van Oeffel <beckoninghook@gmail.com>
2020-10-06 16:43:51 +0200

    added database files

---
+++ app/src/main/java/com/example/madlevel4task1/ProductDao.kt
@@ -0,0 +1,21 @@
+package com.example.madlevel4task1
+
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+interface ProductDao {
+
+    @Query("SELECT * FROM product_table")
+    suspend fun getAllProducts(): List<Product>
+
+    @Insert
+    suspend fun insertProduct(product: Product)
+
+    @Delete
+    suspend fun deleteProduct(product: Product)
+
+    @Query("DELETE FROM product_table")
+    suspend fun deleteAllProducts()
+
+}
